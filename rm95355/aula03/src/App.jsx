65966c35d51ef2a67ca858804b532d3cfac479a9
AraujoAlan <aalan.araujo@hotmail.com>
2022-08-12 22:13:37 -0300

    2o ex useState

--- rm95355/aula03/src/App.jsx
+++ rm95355/aula03/src/App.jsx
@@ -2,21 +2,14 @@ import React,{ useState } from 'react'
 
 export default function App() {
 
-    
-
-    let nome = "alan"
-
-    const mudaNome = () => {
-        nome = nome.toUpperCase()
-        return nome
-    }
+    const [valorState, setValorState] = useState(5)
 
   return (
     <div>
         <h1>ESTUDO SOBRE STATE</h1>
 
-        <button onClick={() => mudaNome()}>ALTERA-NOME</button>
-        <h2>Nome : {nome}</h2>
+        <button onClick={() => setValorState("MARIA")}>ALTERA-NOME</button>
+        <h2>Valor do state : {valorState}</h2>
     </div>
   )
 }
