04f994e9b1634f92b1148bd196998847efa1af49
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-12 22:41:22 -0300

    3o Exemplo de Props com CSS e Obj.

--- pf0670/aula03/src/App.jsx
+++ pf0670/aula03/src/App.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react'
+import TesteFilho from './components/testeFilho/TesteFilho'
 
 export default function App() {
 
     const [valorState, setValorState] = useState()
 
+    //Objeto estiloButton
+    const btnObjEstilo = {
+        color: '#fff',
+        fontSize: '2em',
+        borderRadius: '3px',
+        backgroundColor: '#002300'
+    }
 
     return (
         <div>
-            <h1>ESTUDO SOBRE STATE</h1>
+            <h1 style={{ color: 'yellow', backgroundColor: '#000000', textAlign: 'center' }} >ESTUDO SOBRE STATE</h1>
 
-            <button onClick={() => setValorState("MARIA")}>ALTERA-STATE</button>
+            <button style={btnObjEstilo} onClick={() => setValorState("MARIA")}>ALTERA-STATE</button>
             <h2>Valor do state : {valorState}</h2>
-        </div>
+            <TesteFilho
+                valorStateProps={valorState}
+                setValorStateProps={setValorState}
+                atributoEstiloProps={btnObjEstilo}
+            />
+        </div >
     )
 }
