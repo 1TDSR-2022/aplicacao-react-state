ae6d7bdc202026598aa9705414ded8897e63dde7
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-12 22:13:47 -0300

    2o Exemplo useState

--- pf0670/aula03/src/App.jsx
+++ pf0670/aula03/src/App.jsx
@@ -2,23 +2,15 @@ import React, { useState } from 'react'
 
 export default function App() {
 
+    const [valorState, setValorState] = useState()
 
-
-
-    let nome = "emerson"
-
-    const mudaNone = () => {
-        nome = nome.toUpperCase()
-        console.log(nome)
-        return nome
-    }
 
     return (
         <div>
             <h1>ESTUDO SOBRE STATE</h1>
 
-            <button onClick={() => mudaNone()}>ALTERA-NOME</button>
-            <h2>Nome : {nome}</h2>
+            <button onClick={() => setValorState("MARIA")}>ALTERA-STATE</button>
+            <h2>Valor do state : {valorState}</h2>
         </div>
     )
 }
