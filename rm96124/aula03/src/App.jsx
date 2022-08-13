38549069060cec79f88622c4ad252f63a86b7eca
LuizHPaiola <102267230+LuizHPaiola@users.noreply.github.com>
2022-08-12 21:59:28 -0300

    Primeiro exemplo useState

---
+++ rm96124/aula03/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react"
+
+export default function App() {
+    
+
+    
+    let nome = "Luiz"
+    const mudaNome=()=>{
+        nome = nome.toUpperCase()
+        return nome
+    }
+    return (
+    <div>
+      <h1>ESTUDO SOBRE STATE</h1>
+      <button onClick={()=> mudaNome()}>ALTERA NOME</button>
+      <h2>Nome: {nome} </h2>
+    </div>
+  )
+}
