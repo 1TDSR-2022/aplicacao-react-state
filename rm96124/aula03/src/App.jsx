cfd1ba55cff7ed16b942031b52a187c3f3c77c27
LuizHPaiola <102267230+LuizHPaiola@users.noreply.github.com>
2022-08-12 22:13:25 -0300

    2° exemplo de useState

--- rm96124/aula03/src/App.jsx
+++ rm96124/aula03/src/App.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react"
 
 export default function App() {
+    //setValorState é uma função, e quando for monta, usa set(nome do useState que vc ta usando antes, como no exemplo usamos valorState, fica SetValorState, usando camel case)
+    const [valorState, setValorState] = useState(5)
     
-
     
-    let nome = "Luiz"
-    const mudaNome=()=>{
-        nome = nome.toUpperCase()
-        return nome
-    }
     return (
     <div>
       <h1>ESTUDO SOBRE STATE</h1>
-      <button onClick={()=> mudaNome()}>ALTERA NOME</button>
-      <h2>Nome: {nome} </h2>
+      <button onClick={()=>setValorState(valorState+1)}>ALTERA STATE</button>
+      <h2>Valor do state: {valorState} </h2>
     </div>
   )
 }
