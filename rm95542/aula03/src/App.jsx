5f53c8c2d47e41c719e611c64e176bbeb7f1a454
rm95542 <rm95542@fiap.com.br>
2022-08-12 22:48:28 -0300

    final da aula

--- rm95542/aula03/src/App.jsx
+++ rm95542/aula03/src/App.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react'
+import TesteFilho from './componentes/testeFilho/TesteFilho'
 
 export default function App() {
 
-  const [valorState, setValorState] = useState("Guilherme")
+    const [valorState, setValorState] = useState()
+
+    //Objeto estiloButton
+    const btnObjEstilo = {
+        color: '#fff',
+        fontSize: '2em',
+        borderRadius: '3px',
+        backgroundColor: '#002300'
+    }
 
     return (
         <div>
-           <h1>Estudo sobre STATE</h1>
-           <button onClick={() => setValorState("Maria")}ALTERAR-STATE></button>
-           <h2>valor do state : {valorState}</h2>
+            <h1 style={{ color: 'yellow', backgroundColor: '#000000', textAlign: 'center' }} >ESTUDO SOBRE STATE</h1>
 
-        </div>
-
-  )
+            <button style={btnObjEstilo} onClick={() => setValorState("MARIA")}>ALTERA-STATE</button>
+            <h2>Valor do state : {valorState}</h2>
+            <TesteFilho
+                valorStateProps={valorState}
+                setValorStateProps={setValorState}
+                atributoEstiloProps={btnObjEstilo}
+            />
+        </div >
+    )
 }
