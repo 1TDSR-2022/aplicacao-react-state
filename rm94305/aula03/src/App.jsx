3a4acde61009219419cb7d35be6facad12f95bde
milenaggoes <rm94305@fiap.com.br>
2022-08-25 15:42:18 -0300

    Correção teste filho

--- rm94305/aula03/src/App.jsx
+++ rm94305/aula03/src/App.jsx
@@ -1,29 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import TesteFilho from './components/testeFilho/TesteFilho'
 
-export default function App(){
+export default function App() {
 
-    const[valorState, setValorState] = useState()
-    
+    const [valorState, setValorState] = useState()
+
     //Objeto estiloButton
     const btnObjEstilo = {
-        color:'#fff',
-        fontsize:'2em',
+        color: '#fff',
+        fontSize: '2em',
         borderRadius: '3px',
-        backgroundColor:'#0000ff'
+        backgroundColor: '#002300'
     }
 
-    return(
+    return (
         <div>
-            <h1 style={{color:'yellow', backgroundColor: '#000000', textAlign:'center'}}>ESTUDO SOBRE STATE</h1>
-            <button style={btnObjEstilo} onClick={() =>setValorState("Milena")}>ALTERA-STATE</button>
-            <h2>Valor do state: {valorState}</h2>
+            <h1 style={{ color: 'yellow', backgroundColor: '#000000', textAlign: 'center' }} >ESTUDO SOBRE STATE</h1>
+
+            <button style={btnObjEstilo} onClick={() => setValorState("MARIA")}>ALTERA-STATE</button>
+            <h2>Valor do state : {valorState}</h2>
             <TesteFilho
                 valorStateProps={valorState}
                 setValorStateProps={setValorState}
                 atributoEstiloProps={btnObjEstilo}
-
             />
-        </div>
+        </div >
     )
 }
