33a96e966aff87ba9b937d246ba1db749c326a41
LuisHPerestrelo <rm93738@fiap.com.br>
2022-09-13 23:56:25 -0300

    Exercicio01 - part 1

---
+++ rm93738/aula03/src/components/exercicio01/componente/Corpo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Corpo() {
+     return(
+        <>
+            
+            <h2> Subtitulo </h2>
+            <img src=''></img>
+            <p> acima  </p>
+            <p> tem </p>
+            <p> uma imagem e </p>
+            <p> Subtitulo </p>
+        </>
+
+     ) 
+}
---
+++ rm93738/aula03/src/components/exercicio01/componente/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+     return(
+        <>
+            <header>
+                <h1> Titulo </h1>
+                <p> aqui tem o Header com titulo </p>
+            </header>
+        </>
+
+     ) 
+}
